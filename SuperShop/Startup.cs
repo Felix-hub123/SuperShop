4d662cde12f9fb16cba0b5288789fe936b0041b7
Félix Clemente <Felix.cle@hotmail.com>
2025-05-01 12:38:34 +0100

    SeedDb

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -30,6 +30,9 @@ namespace SuperShop
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddTransient<SeedDb>();
+            
+
             services.AddControllersWithViews();
         }
 
